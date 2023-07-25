57535be0a0ec854d8ad716f48abf64bb0e90b0dc
anshuman010 <ganshul1080@gmail.com>
2023-07-25 14:13:36 +0530

    c

--- Bully/lambda/index.js
+++ Bully/lambda/index.js
@@ -12,7 +12,7 @@ const LaunchRequestHandler = {
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
-            .getResponse();
+            .getResponse(); 
     }
 };
 const HelloWorldIntentHandler = {
